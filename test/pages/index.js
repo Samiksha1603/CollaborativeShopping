548409d3a2a903d483d4b38799bf6ed998d18914
x-ordia <89120526+x-ordia@users.noreply.github.com>
2022-10-05 17:09:07 +0530

    Progress. Integrating react UI in NextJS

--- test/pages/index.js
+++ test/pages/index.js
@@ -1,23 +1,37 @@
 import Head from 'next/head'
 import Script from 'next/script'
-// import { Html, Main } from 'next/document'
 import { useEffect, useState } from 'react'
 import styles from '../styles/Home.module.css'
+// import '../styles/index.module.css';
 import pusher from 'pusher'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './app';
+
+// import {router} from "next/client";
+// import { Html, Main } from 'next/document'
+//react index
+// import reportWebVitals from './reportWebVitals';
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+//end
 
 export default function Home() {
   const [username, setUsername] = useState('');
   const [messages, setMessages] = useState([]);
   const [message, setMessage] = useState('');
   let allMessages = [];
+  if (typeof window !== "undefined") {
+
+  }
 
   useEffect(() => {
     Pusher.logToConsole = true;
-
     const pusher = new Pusher('361f7ef1d7f18076912c', {
       cluster: 'ap2'
     });
-
     const channel = pusher.subscribe('test');
     channel.bind('message', function(data) {
       allMessages.push(data);
@@ -39,44 +53,54 @@ export default function Home() {
     });
     console.log(response);
     setMessage('');
-
   }
+
+  //deleted :
+  // <script src="https://unpkg.com/react/umd/react.production.min.js" crossorigin></script>
+  // <script src="https://unpkg.com/react-dom/umd/react-dom.production.min.js" crossorigin></script>
+  // <script src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js" crossorigin></script>
 
   return (
     <div className={styles.container}>
       <Head>
         <title>Colab</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
       </Head>
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous"/>
-      <script src="https://js.pusher.com/7.2/pusher.min.js"></script>
+      <noscript>You need to enable JavaScript to run this app.</noscript>
+      <React.StrictMode>
+        <App />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous"/>
+        <script src="https://js.pusher.com/7.2/pusher.min.js"></script>
+        <div className="d-flex flex-column align-items-stretch flex-shrink-0 bg-white" className={styles.chat}>
+          <div className="d-flex align-items-center flex-shrink-0 p-3 link-dark text-decoration-none border-bottom">
+            <span >Enter username : </span>
+            <input className="fs-5 fw-semibold" placeholder='Username' value={username} onChange={e => setUsername(e.target.value)}/>
+          </div>
 
-      <div className="d-flex flex-column align-items-stretch flex-shrink-0 bg-white">
-        <div className="d-flex align-items-center flex-shrink-0 p-3 link-dark text-decoration-none border-bottom">
-          <span >Enter username : </span>
-          <input className="fs-5 fw-semibold" placeholder='Username' value={username} onChange={e => setUsername(e.target.value)}/>
-        </div>
-
-        <div className="list-group list-group-flush border-bottom scrollarea" style={{minHeight: "500px"}}>
-          {messages.map(message => {
-            return(
-              <div className="list-group-item list-group-item-action py-3 lh-tight">
-                <div className="d-flex w-100 align-items-center justify-content-between">
-                  <strong className="mb-1">{message.username}</strong>
+          <div className="list-group list-group-flush border-bottom scrollarea" style={{minHeight: "200px"}}>
+            {messages.map(message => {
+              return(
+                <div className="list-group-item list-group-item-action py-3 lh-tight">
+                  <div className="d-flex w-100 align-items-center justify-content-between">
+                    <strong className="mb-1">{message.username}</strong>
+                  </div>
+                  <div className="col-10 mb-1 small">{message.message}</div>
                 </div>
-                <div className="col-10 mb-1 small">{message.message}</div>
-              </div>
-            )
-          })}
+              )
+            })}
+          </div>
         </div>
-      </div>
 
-      <form onSubmit={submit}>
-        <input className='form-control' placeholder='Enter your message' value = {message}
-            onChange={e => setMessage(e.target.value)}
-        />
-        <button type='submit'>Submit</button>
-      </form>
-
+        <form onSubmit={submit} className={styles.chat}>
+          <input className='form-control' placeholder='Enter your message' value = {message}
+              onChange={e => setMessage(e.target.value)}
+          />
+          <button type='submit'>Submit</button>
+        </form>
+      </React.StrictMode>
+      <script>var Alert = ReactBootstrap.Alert;</script>
     </div>
   )
 }
